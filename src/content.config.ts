f41afded543da98bab641048c843e829ed5b31ea
RelativeSure <mail@rasmusj.dk>
2025-01-17 22:26:58 +0100

    Init

---
+++ src/content.config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const pages = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/pages" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    //pubDate: z.coerce.date(),
+  }),
+});
+
+export const collections = { pages };
